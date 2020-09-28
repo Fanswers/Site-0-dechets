682de839f466fc32bf1d23ac0b66fdf7e03160c0
AlexisL <alexis.larrode@gmail.com>
2020-09-28 10:27:12 +0200

    Visuel page annonce

---
+++ user_advert.php
@@ -0,0 +1,57 @@
+<?php require_once("user_advert_head.inc.php")?>
+<body>
+
+    <div class="container-lg">
+        <div class="imageContainer">
+            <div class="bigImage">
+                <img src="https://via.placeholder.com/380x320" alt="">
+            </div>
+            <div class="littleImage">
+                <div class="littleImage1">
+                    <img src="https://via.placeholder.com/164x140" alt="">
+                    <img src="https://via.placeholder.com/164x140" alt="">
+                </div>
+                <div class="littleImage2">
+                    <img src="https://via.placeholder.com/164x140" alt="">
+                    <img src="https://via.placeholder.com/164x140" alt="">
+                </div>
+            </div>
+        </div>
+        <div class="advertInfo">
+            <div class="advertName">
+                <h2>Titre de l'annonce</h2>
+            </div>
+            <div class="advertDesc">
+                <p>Iam virtutem ex consuetudine vitae sermonisque nostri interpretemur nec eam, ut quidam docti, verborum magnificentia metiamur virosque bonos eos, qui habentur, numeremus, Paulos, Catones, Galos, Scipiones, Philos; his communis vita contenta est; eos autem omittamus, qui omnino nusquam reperiuntur.<br><br>
+    
+                    Restabat ut Caesar post haec properaret accitus et abstergendae causa suspicionis sororem suam, eius uxorem, Constantius ad se tandem desideratam venire multis fictisque blanditiis hortabatur. quae licet ambigeret metuens saepe cruentum, spe tamen quod eum lenire poterit ut germanum profecta, cum Bithyniam introisset, in statione quae Caenos Gallicanos appellatur, absumpta est vi febrium repentina. cuius post obitum maritus contemplans cecidisse fiduciam qua se fultum existimabat, anxia cogitatione, quid moliretur haerebat.<br><br>
+                    
+                    Sed laeditur hic coetuum magnificus splendor levitate paucorum incondita, ubi nati sunt non reputantium, sed tamquam indulta licentia vitiis ad errores lapsorum ac lasciviam. ut enim Simonides lyricus docet, beate perfecta ratione vieturo ante alia patriam esse convenit gloriosam.
+                </p>
+            </div>
+            <div class="advertLocalisation">
+                <img src="https://via.placeholder.com/960x500" alt="">
+            </div>
+        </div>
+        <div class="advertComment">
+            <div class="inlineTitleButton">
+                <h3 class=commentTitle>Commentaires</h3>
+                <button type="button" class="btn btn-outline-secondary" id="commentBtn">Ajouter commentaire</button>
+            </div>
+            <div class="jumbotron" id="allComment">
+                <p>Pseudo</p>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum sint, illo nesciunt voluptates, expedita hic ducimus cumque tenetur, vero totam mollitia soluta pariatur. Sit optio, incidunt sed a laborum perspiciatis.</p><br>
+                <p>Pseudo</p>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum sint, illo nesciunt voluptates, expedita hic ducimus cumque tenetur, vero totam mollitia soluta pariatur. Sit optio, incidunt sed a laborum perspiciatis.</p><br>
+                <p>Pseudo</p>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum sint, illo nesciunt voluptates, expedita hic ducimus cumque tenetur, vero totam mollitia soluta pariatur. Sit optio, incidunt sed a laborum perspiciatis.</p><br>
+            </div> 
+        </div>
+        <div class="contactAdvertiser">
+            <button type="button" class="btn btn-outline-secondary" id="contactAdvertiserBtn">Contacter l'annonceur</button>
+            <textarea class="form-control" id="exampleFormControlTextarea" rows="3"></textarea>
+        </div>   
+    </div>  
+
+</body>
+</html>
